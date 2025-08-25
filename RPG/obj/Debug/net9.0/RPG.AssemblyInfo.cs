5d4e256f8c565b51ffdf65f2fd9c46fdfbc4b460
William Abreu <abreuwilliam>
2025-08-25 20:31:57 -0300

    atualizando version

--- RPG/obj/Debug/net9.0/RPG.AssemblyInfo.cs
+++ RPG/obj/Debug/net9.0/RPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44b217fd712f0a16fbd9e3d1179f67a9764fd7c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a8fc7c763fb737fd83c772f810af789e6fab3ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
