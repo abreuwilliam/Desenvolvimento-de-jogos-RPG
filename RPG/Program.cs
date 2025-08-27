c376ecf4d501ff133409d82f2dfdfa5c460cd56a
William <131878264+abreuwilliam@users.noreply.github.com>
2025-08-27 17:07:56 -0300

    ajuste de personagem

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -1,55 +1,22 @@
 ﻿﻿using System;
 using System.Threading;
-using Rpg.Principal.Abstracts;
-using Rpg.Principal.Personagens;
+using Rpg.Classes.Abstracts;
+using Rpg.Classes.Personagens;
 
-<<<<<<< HEAD
 Console.WriteLine("Hello, World!");
 Personagem Protagonista = new Personagem("Herói", nivel: 2, ataque: 150, defesa: 50);
-Lobo lobo = new Lobo();
+LoboSombrio lobo = new LoboSombrio();
 Combate combate = new Combate(Protagonista, lobo);
-=======
+
 Console.WriteLine("💥 Batalha: Herói vs Vilão");
->>>>>>> 335c58431b6fa26479ab7b87d4a3b143ac94e5f5
+
 
 Heroi heroi = new Heroi("Arthur", 10, 15, 5);
-Vilao vilao = new Vilao("Orc", 5, 12, 4);
+Crocodilo crocodilo = new Crocodilo();
 
 heroi.MostrarStatus();
-vilao.MostrarStatus();
-
-// Batalha contra o Vilão
-while (heroi.EstaVivo && vilao.EstaVivo)
-{
-    heroi.AtacarAlvo(vilao);
-
-    if (vilao.EstaVivo)
-        vilao.AtacarAlvo(heroi);
-    else
-        vilao.ConcederRecompensa(heroi);
-
-    Thread.Sleep(1000);
-}
-
-// Nemesis aparece
-if (heroi.EstaVivo)
-{
-    Console.WriteLine("\n🌑 Um inimigo misterioso surge do nada! É o Nemesis!");
-    Lobo nemesis = new Lobo("Lobo Negro");
-
-    while (heroi.EstaVivo && nemesis.EstaVivo)
-    {
-        heroi.AtacarAlvo(nemesis);
-
-        if (nemesis.EstaVivo)
-            nemesis.AtacarAlvo(heroi);
-        else
-            nemesis.ConcederRecompensa(heroi);
-
-        Thread.Sleep(1000);
-    }
-}
-
+crocodilo.MostrarStatus();
+combate.Iniciar();
 heroi.MostrarStatus();
 Console.WriteLine("🏆 Aventura encerrada!");
 Console.ReadLine();
