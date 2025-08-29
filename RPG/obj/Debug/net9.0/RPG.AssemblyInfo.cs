c03c30e358b890302fda7521d3cd5059ffddb71f
William <131878264+abreuwilliam@users.noreply.github.com>
2025-08-29 18:17:08 -0300

    versio

--- RPG/obj/Debug/net9.0/RPG.AssemblyInfo.cs
+++ RPG/obj/Debug/net9.0/RPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+175c1278d47808bb0a743e3717478a0696c74a21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a30c852ab6007992f0e10d5307b9c9d865b2cde")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
