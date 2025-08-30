c8296f6eb99241359adff49656b8beec9056da8c
Caio Espindola <131320779+caioespinndola@users.noreply.github.com>
2025-08-29 23:10:06 -0300

    adicao missao aranha

---
+++ RPG/Classes/Personagens/AranhaGigante.cs
@@ -0,0 +1,13 @@
+﻿using Rpg.Classes.Abstracts;
+
+namespace Rpg.Classes.Personagens
+{
+    public class AranhaGigante : Personagem
+    {
+        public AranhaGigante(string nome = "Aranha Gigante")
+            : base(nome, nivel: 42, ataque: 490, defesa: 1100)
+        {
+        }
+
+    }
+}
